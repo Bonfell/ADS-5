764652a078e892605b416dd8c327d874b1a7fb22
Bonfell <porkhunov99@inbox.ru>
2026-04-18 22:21:01 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,48 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#ifndef TSTACK_H
+#define TSTACK_H
 
-template<typename T, int size>
+#include <stdexcept>
+
+template<typename T, size_t Capacity>
 class TStack {
-  // добавьте код стека
-};
+private:
+    T data[Capacity];
+    size_t topIndex;
 
+public:
+    TStack() : topIndex(0) {}
+
+    bool isEmpty() const {
+        return topIndex == 0;
+    }
+
+    bool isFull() const {
+        return topIndex >= Capacity;
+    }
+
+    void push(const T& value) {
+        if (!isFull()) {
+            data[topIndex++] = value;
+        } else {
+            throw std::overflow_error("Stack overflow");
+        }
+    }
+
+    T pop() {
+        if (!isEmpty()) {
+            return data[--topIndex];
+        } else {
+            throw std::underflow_error("Stack underflow");
+        }
+    }
+
+    T peek() const {
+        if (!isEmpty()) {
+            return data[topIndex - 1];
+        } else {
+            throw std::underflow_error("Stack is empty");
+        }
+    }
+};
 #endif  // INCLUDE_TSTACK_H_
